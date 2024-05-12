a5ef0572eceff54441af27b72c1d71fce1797228
maddrills <mathewfvv@gmail.com>
2024-05-13 00:22:46 +0530

    First table

--- The Application/db/MySQL/Mathew's_Bank.sql
+++ The Application/db/MySQL/Mathew's_Bank.sql
@@ -1,2 +1,12 @@
 -- E:\Mega Project\Mathew-s_secure_bank\The Application\db\MySQL
 -- new db brantch 
+CREATE DATABASE IF NOT EXISTS `mathew_bank_db`;
+
+USE `mathew_bank_db`;
+
+-- foreign key to be added to this table in alter table form  
+CREATE TABLE `user`(
+`u_id` INTEGER NOT NULL AUTO_INCREMENT,
+`user_name` VARCHAR(70) NOT NULL,
+`password` VARCHAR(68) NOT NULL
+);
