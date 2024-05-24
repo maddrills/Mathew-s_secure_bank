6e872120af79c3e50be81daf4c997229dd05ce1a
maddrills <mathewfvv@gmail.com>
2024-05-24 21:37:54 +0530

    To Do

--- The Application/back-end/spring/Mathew's-bank-back/Mathew-bank/src/main/java/com/mathew/bank/Mathewbank/service/AdminService.java
+++ The Application/back-end/spring/Mathew's-bank-back/Mathew-bank/src/main/java/com/mathew/bank/Mathewbank/service/AdminService.java
@@ -2,7 +2,6 @@ package com.mathew.bank.Mathewbank.service;
 
 import com.mathew.bank.Mathewbank.DAO.EmpRepo;
 import com.mathew.bank.Mathewbank.entity.commonEntity.Role;
-import com.mathew.bank.Mathewbank.entity.employeeOnlyEntity.Branch;
 import com.mathew.bank.Mathewbank.entity.employeeOnlyEntity.employees.Employee;
 import com.mathew.bank.Mathewbank.entity.employeeOnlyEntity.employees.EmployeeDetails;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,15 +29,7 @@ public class AdminService {
             String phone_number, String full_name, String email, Date dateOfBirth, double salary,
             String password, Collection<Role> roles
     ){
-
-//            public EmployeeDetails(String phone_number, String full_name, String email, Date dateOfBirth, double salary, Employee emp_id) {
-//            this.phone_number = phone_number;
-//            this.full_name = full_name;
-//            this.email = email;
-//            this.dateOfBirth = dateOfBirth;
-//            this.salary = salary;
-//            this.emp_id = emp_id;
-//        }
+        //TODO a sanity check to make sure that user enters intended credentials
 
         Employee employee = new Employee(
                 password,
