3c177a9d5285183b753473b45836829a9646aa3d
maddrills <mathewfvv@gmail.com>
2024-05-27 17:27:41 +0530

    All claases nessary for JPA added

---
+++ The Application/back-end/spring/Mathew's-bank-back/Mathew-bank/src/main/java/com/mathew/bank/Mathewbank/entity/WebSightVisited.java
@@ -0,0 +1,5 @@
+package com.mathew.bank.Mathewbank.entity;
+
+public class WebSightVisited {
+}
+
---
+++ The Application/back-end/spring/Mathew's-bank-back/Mathew-bank/src/main/java/com/mathew/bank/Mathewbank/entity/commonEntity/UserApplication.java
@@ -0,0 +1,4 @@
+package com.mathew.bank.Mathewbank.entity.commonEntity;
+
+public class UserApplication {
+}
