7fb4192ca0db04bd2c0831ad777248777e8bc88b
maddrills <mathewfvv@gmail.com>
2024-06-14 17:49:43 +0530

    Initial Home page

--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/app.component.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WelcomePageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'mathewBank';
