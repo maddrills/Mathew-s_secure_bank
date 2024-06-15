7463ec45327bd40c4334ea9fcb72075259abb689
maddrills <mathewfvv@gmail.com>
2024-06-16 00:08:19 +0530

    Footer

---
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/top-down/footer-section/footer-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-section.component.html',
+  styleUrl: './footer-section.component.css'
+})
+export class FooterSectionComponent {
+
+}
--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/welcome-page/welcome-page.component.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/welcome-page/welcome-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../top-down/nav-bar/nav-bar.component';
+import { FooterSectionComponent } from '../top-down/footer-section/footer-section.component';
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, FooterSectionComponent],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css',
 })
