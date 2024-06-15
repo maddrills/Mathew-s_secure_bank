a1b3190da8e81ac1dc76996b8c47378bc96ae791
maddrills <mathewfvv@gmail.com>
2024-06-15 16:32:20 +0530

    refactoring for isolation of nav bar

--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/welcome-page/welcome-page.component.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/welcome-page/welcome-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../top-down/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './welcome-page.component.html',
-  styleUrl: './welcome-page.component.css'
+  styleUrl: './welcome-page.component.css',
 })
-export class WelcomePageComponent {
-
-}
+export class WelcomePageComponent {}
