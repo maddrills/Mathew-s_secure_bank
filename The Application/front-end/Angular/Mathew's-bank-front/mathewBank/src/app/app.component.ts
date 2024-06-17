06c7cbdc454d1767d1556ca15f36f073244a4649
maddrills <mathewfvv@gmail.com>
2024-06-17 16:35:45 +0530

    Log in form

--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/app.component.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 import { ApplyForAccountComponent } from './components/apply-for-account/apply-for-account.component';
+import { LogInPageComponent } from './components/log-in-page/log-in-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomePageComponent, ApplyForAccountComponent],
+  imports: [RouterOutlet, WelcomePageComponent, ApplyForAccountComponent,LogInPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/log-in-page/log-in-page.component.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/log-in-page/log-in-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterSectionComponent } from '../top-down/footer-section/footer-section.component';
+import { NavBarComponent } from '../top-down/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-log-in-page',
   standalone: true,
-  imports: [],
+  imports: [FooterSectionComponent, NavBarComponent],
   templateUrl: './log-in-page.component.html',
-  styleUrl: './log-in-page.component.css'
+  styleUrl: './log-in-page.component.css',
 })
-export class LogInPageComponent {
-
-}
+export class LogInPageComponent {}
