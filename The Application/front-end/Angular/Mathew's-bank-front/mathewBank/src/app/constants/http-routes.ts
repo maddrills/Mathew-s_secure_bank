5e74743d4a8e0ad206288e8600871a6b051e0d5f
maddrills <mathewfvv@gmail.com>
2024-06-19 18:02:23 +0530

    read - onnly for file

--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/constants/http-routes.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/constants/http-routes.ts
@@ -1,13 +1,13 @@
 export class ApplicationHttpRoutes {
-  private static PROTOCOL: string = 'http';
-  private static BACKEND_SERVER: string = 'localhost';
-  private static PORT_NUMBER: string = '8080';
-  private static SOCKET: string =
+  private static readonly PROTOCOL: string = 'http';
+  private static readonly BACKEND_SERVER: string = 'localhost';
+  private static readonly PORT_NUMBER: string = '8080';
+  private static readonly SOCKET: string =
     this.PROTOCOL + '://' + this.BACKEND_SERVER + ':' + this.PORT_NUMBER;
 
   // log in route routes employee and user
-  static LOGIN_ROUTE_EMPLOYEE: string =
+  static readonly LOGIN_ROUTE_EMPLOYEE: string =
     this.SOCKET + '/employee/employee-login';
 
-  static LOGIN_ROUTE_USER: string = this.SOCKET + '/bankUser/login';
+  static readonly LOGIN_ROUTE_USER: string = this.SOCKET + '/bankUser/login';
 }
