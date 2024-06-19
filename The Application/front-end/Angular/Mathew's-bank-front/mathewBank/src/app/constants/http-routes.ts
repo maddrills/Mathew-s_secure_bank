0fdf437f6b7e18f05a2532f51376c21b7632c277
maddrills <mathewfvv@gmail.com>
2024-06-19 17:54:44 +0530

    Constens normalisation

--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/constants/http-routes.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/constants/http-routes.ts
@@ -1,11 +1,13 @@
 export class ApplicationHttpRoutes {
-  private static PROTOCOL: string = 'http://';
-  private static BACKEND_SERVER: string = 'localhost:8080';
+  private static PROTOCOL: string = 'http';
+  private static BACKEND_SERVER: string = 'localhost';
+  private static PORT_NUMBER: string = '8080';
+  private static SOCKET: string =
+    this.PROTOCOL + '://' + this.BACKEND_SERVER + ':' + this.PORT_NUMBER;
 
   // log in route routes employee and user
   static LOGIN_ROUTE_EMPLOYEE: string =
-    this.PROTOCOL + this.BACKEND_SERVER + '/employee/employee-login';
+    this.SOCKET + '/employee/employee-login';
 
-  static LOGIN_ROUTE_USER: string =
-    this.PROTOCOL + this.BACKEND_SERVER + '/bankUser/login';
+  static LOGIN_ROUTE_USER: string = this.SOCKET + '/bankUser/login';
 }
