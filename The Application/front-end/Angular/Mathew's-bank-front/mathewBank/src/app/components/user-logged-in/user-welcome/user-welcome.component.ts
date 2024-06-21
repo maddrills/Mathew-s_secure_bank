2f54e49548e3ec268296abfd297b2b2e5e66bf34
maddrills <mathewfvv@gmail.com>
2024-06-21 13:41:28 +0530

    Arrow up down logic

--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/user-logged-in/user-welcome/user-welcome.component.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/user-logged-in/user-welcome/user-welcome.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../top-down/nav-bar/nav-bar.component';
 import { FooterSectionComponent } from '../../top-down/footer-section/footer-section.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-welcome',
   standalone: true,
-  imports: [NavBarComponent, FooterSectionComponent],
+  imports: [NavBarComponent, FooterSectionComponent, CommonModule],
   templateUrl: './user-welcome.component.html',
   styleUrl: './user-welcome.component.css',
 })
 export class UserWelcomeComponent {
   userName = 'Mathew Francis';
+
+  savingsVisible: boolean = false;
+  checkingVisible: boolean = false;
+  buildUpVisible: boolean = false;
+
+  visibilityInverter(arg0: boolean) {
+    arg0 = !arg0;
+  }
 }
