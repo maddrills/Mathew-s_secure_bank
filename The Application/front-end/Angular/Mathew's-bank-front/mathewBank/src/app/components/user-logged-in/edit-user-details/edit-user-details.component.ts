76d38b19aa09bf080c31d35fcbc5ef0f2b65c435
maddrills <mathewfvv@gmail.com>
2024-06-24 17:45:07 +0530

    Ui fix with router nav to user home and triangle rotation

--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/user-logged-in/edit-user-details/edit-user-details.component.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/user-logged-in/edit-user-details/edit-user-details.component.ts
@@ -1,20 +1,36 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../top-down/nav-bar/nav-bar.component';
 import { FooterSectionComponent } from '../../top-down/footer-section/footer-section.component';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-user-details',
   standalone: true,
-  imports: [NavBarComponent, FooterSectionComponent],
+  imports: [NavBarComponent, FooterSectionComponent, CommonModule],
   templateUrl: './edit-user-details.component.html',
   styleUrl: './edit-user-details.component.css',
 })
 export class EditUserDetailsComponent {
-  private startPoint: boolean = true;
+  constructor(private router: Router) {}
+
+  private startPoint: boolean = false;
 
   moneyTransferActive: boolean = this.startPoint;
   transactions: boolean = this.startPoint;
   editSavingsAccount: boolean = this.startPoint;
   inactiveAccount: boolean = this.startPoint;
   closeAccount: boolean = this.startPoint;
+
+  editSavingsAccountLogic() {
+    this.editSavingsAccount = !this.editSavingsAccount;
+    if (!this.editSavingsAccount) {
+      this.inactiveAccount = false;
+      this.closeAccount = false;
+    }
+  }
+
+  backToUserHome() {
+    this.router.navigate(['user-welcome']);
+  }
 }
