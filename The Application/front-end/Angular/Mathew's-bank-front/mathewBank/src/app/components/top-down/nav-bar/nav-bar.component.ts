8f631223385ac30920168d4be2484821e82223e5
maddrills <mathewfvv@gmail.com>
2024-06-24 20:47:16 +0530

    Nav bar logic

--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/top-down/nav-bar/nav-bar.component.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/top-down/nav-bar/nav-bar.component.ts
@@ -1,20 +1,51 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UnRegService } from '../../../service/unRegService';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
 export class NavBarComponent {
+  public loginActive: boolean = false;
+
+  public mathewBackHome: boolean = false;
+  public loginSelected: boolean = false;
+
+  constructor(
+    private router: Router,
+    private unRegService: UnRegService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    unRegService.userIsLoggedIn.subscribe(
+      (userIn) => (this.loginActive = userIn)
+    );
+    console.log(`nav bar ${this.loginActive}`);
+
+    const url = this.router.url;
+    if (url === '/log-in') {
+      this.loginSelected = true;
+    } else if (url === '/welcome') {
+      this.mathewBackHome = true;
+    }
+  }
+
   loginPage() {
     this.router.navigate(['log-in']);
   }
-  constructor(private router: Router) {}
 
   homePageRedirect() {
+    this.router.navigate(['welcome']);
+  }
+
+  logOutPage() {
+    //emit a logout that user is no longer longed in
+    this.unRegService.userIsLoggedIn.next(false);
+    this.loginActive = false;
     this.router.navigate(['welcome']);
   }
 }
