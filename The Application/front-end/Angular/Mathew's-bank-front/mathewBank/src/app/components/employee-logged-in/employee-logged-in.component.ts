e8ecbfbc61056828de4ad7c51ef4de2ffab2ab74
maddrills <mathewfvv@gmail.com>
2024-07-05 16:46:53 +0530

    Assigned applications created

--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/employee-logged-in/employee-logged-in.component.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/employee-logged-in/employee-logged-in.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from '../top-down/nav-bar/nav-bar.component';
 import { FooterSectionComponent } from '../top-down/footer-section/footer-section.component';
 import { BankApplicationsComponent } from './bank-applications/bank-applications.component';
+import { AssignedApplicationsComponent } from './assigned-applications/assigned-applications.component';
 
 @Component({
   selector: 'app-employee-logged-in',
   standalone: true,
-  imports: [NavBarComponent, FooterSectionComponent, BankApplicationsComponent],
+  imports: [
+    NavBarComponent,
+    FooterSectionComponent,
+    BankApplicationsComponent,
+    AssignedApplicationsComponent,
+  ],
   templateUrl: './employee-logged-in.component.html',
   styleUrl: './employee-logged-in.component.css',
 })
