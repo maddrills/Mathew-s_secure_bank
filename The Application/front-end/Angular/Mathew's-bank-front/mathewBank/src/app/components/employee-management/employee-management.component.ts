75f966201823511fbe090bdc752e3b3402dc4777
maddrills <mathewfvv@gmail.com>
2024-07-05 19:57:08 +0530

    Update

--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/employee-management/employee-management.component.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/employee-management/employee-management.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { EmployeeViewComponent } from './employee-view/employee-view.component';
 import { AssignedApplicationsComponent } from '../employee-logged-in/assigned-applications/assigned-applications.component';
+import { SubEmployeesComponent } from './sub-employees/sub-employees.component';
 
 @Component({
   selector: 'app-employee-management',
   standalone: true,
-  imports: [EmployeeViewComponent, AssignedApplicationsComponent],
+  imports: [
+    EmployeeViewComponent,
+    AssignedApplicationsComponent,
+    SubEmployeesComponent,
+  ],
   templateUrl: './employee-management.component.html',
   styleUrl: './employee-management.component.css',
 })
--- The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/employee-management/sub-employees/sub-employees.component.ts
+++ The Application/front-end/Angular/Mathew's-bank-front/mathewBank/src/app/components/employee-management/sub-employees/sub-employees.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { AssignedApplicationsComponent } from '../../employee-logged-in/assigned-applications/assigned-applications.component';
+import { EmployeeManagementComponent } from '../employee-management.component';
 
 @Component({
   selector: 'app-sub-employees',
   standalone: true,
-  imports: [],
+  imports: [AssignedApplicationsComponent, EmployeeManagementComponent],
   templateUrl: './sub-employees.component.html',
-  styleUrl: './sub-employees.component.css'
+  styleUrl: './sub-employees.component.css',
 })
-export class SubEmployeesComponent {
-
-}
+export class SubEmployeesComponent {}
